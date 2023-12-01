634bc55df6363d3c13f08ac52fdca65581b35487
Jacob <hortonjacob1@gmail.com>
2023-12-01 21:47:39 +0000

    refactor: make much nicer

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,15 +1,44 @@
+static WORD_NUMBERS: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
 fn main() {
     let input = include_str!("../inputs/input.txt");
     let result = process(input);
     println!("{result}");
 }
 
-// TODO: tidy
+fn find_number(line: &str, from_end: bool) -> u32 {
+    let mut char_indices: Vec<(usize, char)> = line.char_indices().collect();
+    if from_end {
+        char_indices = char_indices.into_iter().rev().collect();
+    }
+
+    for (i, char) in char_indices {
+        // Check if there is a spelled out number
+        let word_number = WORD_NUMBERS.iter().find(|w| {
+            if from_end {
+                line[i..].starts_with(*w)
+            } else {
+                line[..i].ends_with(*w)
+            }
+        });
+        if let Some(word_number) = word_number {
+            let digit = WORD_NUMBERS.iter().position(|w| w == word_number).unwrap();
+            return digit as u32 + 1; // Add 1 because 0 indexed
+        }
+
+        // Check for digit
+        if char.is_numeric() {
+            return char.to_digit(10).unwrap();
+        }
+    }
+
+    panic!("Shouldn't happen. Invalid input - no numbers found")
+}
+
 fn process(input: &str) -> u32 {
     let lines = input.split('\n');
-    let word_numbers = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
 
     lines
         .map(|line| {
@@ -17,62 +46,10 @@ fn process(input: &str) -> u32 {
                 return 0;
             }
 
-            let first_numeric_i = line.chars().position(|c: char| c.is_numeric());
-            let last_numeric_i = line
-                .chars()
-                .rev()
-                .position(|c: char| c.is_numeric())
-                .map(|i| line.len() - i - 1);
+            let first = find_number(line, false);
+            let last = find_number(line, true);
 
-            let first_word = word_numbers
-                .iter()
-                .filter_map(|word_number| Some((word_number, line.find(word_number)?)))
-                .min_by(|(_, i1), (_, i2)| i1.cmp(i2));
-
-            let last_word = word_numbers
-                .iter()
-                .filter_map(|word_number| Some((word_number, line.rfind(word_number)?)))
-                .max_by(|(_, i1), (_, i2)| i1.cmp(i2));
-
-            let first = match first_word {
-                Some((w, i)) => {
-                    let mut val = None;
-                    if let Some(first_numeric_i) = first_numeric_i {
-                        if i > first_numeric_i {
-                            val = line.chars().nth(first_numeric_i).unwrap().to_digit(10);
-                        }
-                    }
-
-                    val.unwrap_or(word_numbers.iter().position(|x| x == w).unwrap() as u32 + 1)
-                }
-                None => line
-                    .chars()
-                    .nth(first_numeric_i.unwrap())
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap(),
-            };
-
-            let last = match last_word {
-                Some((w, i)) => {
-                    let mut val = None;
-                    if let Some(last_numeric_i) = last_numeric_i {
-                        if i < last_numeric_i {
-                            val = line.chars().nth(last_numeric_i).unwrap().to_digit(10);
-                        }
-                    }
-
-                    val.unwrap_or(word_numbers.iter().position(|x| x == w).unwrap() as u32 + 1)
-                }
-                None => line
-                    .chars()
-                    .nth(last_numeric_i.unwrap())
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap(),
-            };
-
-            format!("{first}{last}").parse::<u32>().unwrap()
+            first * 10 + last
         })
         .sum()
 }
