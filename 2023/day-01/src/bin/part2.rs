d8e61d7297727a5cb2f0e9a9429ed6577f00788b
Jacob <hortonjacob1@gmail.com>
2023-12-04 19:39:04 +0000

    alternative implementation for day 1 part 2

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -1,43 +1,21 @@
-static WORD_NUMBERS: [&'static str; 9] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
+use std::env::args;
+
+use day_01::{LinearSearchNumberFinder, NumberFinder, ReplaceNumberFinder};
 
 fn main() {
+    let finder: Box<dyn NumberFinder> = match args().nth(1).unwrap_or("linear".to_string()).as_str()
+    {
+        "linear" => Box::new(LinearSearchNumberFinder {}),
+        "replace" => Box::new(ReplaceNumberFinder {}),
+        _ => panic!("Invalid number finder. Can be 'linear' or 'replace'"),
+    };
+
     let input = include_str!("../inputs/input.txt");
-    let result = process(input);
+    let result = process(input, finder);
     println!("{result}");
 }
 
-fn find_number(line: &str, from_end: bool) -> u32 {
-    let mut char_indices: Vec<(usize, char)> = line.char_indices().collect();
-    if from_end {
-        char_indices = char_indices.into_iter().rev().collect();
-    }
-
-    for (i, char) in char_indices {
-        // Check if there is a spelled out number
-        let word_number = WORD_NUMBERS.iter().find(|w| {
-            if from_end {
-                line[i..].starts_with(*w)
-            } else {
-                line[..i].ends_with(*w)
-            }
-        });
-        if let Some(word_number) = word_number {
-            let digit = WORD_NUMBERS.iter().position(|w| w == word_number).unwrap();
-            return digit as u32 + 1; // Add 1 because 0 indexed
-        }
-
-        // Check for digit
-        if char.is_numeric() {
-            return char.to_digit(10).unwrap();
-        }
-    }
-
-    panic!("Shouldn't happen. Invalid input - no numbers found")
-}
-
-fn process(input: &str) -> u32 {
+fn process(input: &str, number_finder: Box<dyn NumberFinder>) -> u32 {
     let lines = input.split('\n');
 
     lines
@@ -46,8 +24,8 @@ fn process(input: &str) -> u32 {
                 return 0;
             }
 
-            let first = find_number(line, false);
-            let last = find_number(line, true);
+            let first = number_finder.find_number(line, false);
+            let last = number_finder.find_number(line, true);
 
             first * 10 + last
         })
@@ -56,44 +34,86 @@ fn process(input: &str) -> u32 {
 
 #[cfg(test)]
 mod tests {
+    use day_01::ReplaceNumberFinder;
+
     use super::*;
 
     #[test]
-    fn test_input() {
+    fn test_input_linear() {
         let input = include_str!("../inputs/test_part2.txt");
-        let result = process(input);
+        let result = process(input, Box::new(LinearSearchNumberFinder {}));
 
         assert_eq!(result, 281)
     }
 
     #[test]
-    fn overlapping_word_numbers() {
+    fn overlapping_word_numbers_linear() {
         let input = "afiveighttwo565twonea";
-        let result = process(input);
+        let result = process(input, Box::new(LinearSearchNumberFinder {}));
 
         assert_eq!(result, 51)
     }
 
     #[test]
-    fn two_of_same_digit() {
+    fn two_of_same_digit_linear() {
         let input = "afivetwo565asdf6";
-        let result = process(input);
+        let result = process(input, Box::new(LinearSearchNumberFinder {}));
 
         assert_eq!(result, 56)
     }
 
     #[test]
-    fn two_of_same_word_number() {
+    fn two_of_same_word_number_linear() {
         let input = "a1two56two";
-        let result = process(input);
+        let result = process(input, Box::new(LinearSearchNumberFinder {}));
 
         assert_eq!(result, 12)
     }
 
     #[test]
-    fn real_input() {
+    fn real_input_linear() {
         let input = include_str!("../inputs/input.txt");
-        let result = process(input);
+        let result = process(input, Box::new(LinearSearchNumberFinder {}));
+
+        assert_eq!(result, 54980)
+    }
+
+    #[test]
+    fn test_input_replace() {
+        let input = include_str!("../inputs/test_part2.txt");
+        let result = process(input, Box::new(ReplaceNumberFinder {}));
+
+        assert_eq!(result, 281)
+    }
+
+    #[test]
+    fn overlapping_word_numbers_replace() {
+        let input = "afiveighttwo565twonea";
+        let result = process(input, Box::new(ReplaceNumberFinder {}));
+
+        assert_eq!(result, 51)
+    }
+
+    #[test]
+    fn two_of_same_digit_replace() {
+        let input = "afivetwo565asdf6";
+        let result = process(input, Box::new(ReplaceNumberFinder {}));
+
+        assert_eq!(result, 56)
+    }
+
+    #[test]
+    fn two_of_same_word_number_replace() {
+        let input = "a1two56two";
+        let result = process(input, Box::new(ReplaceNumberFinder {}));
+
+        assert_eq!(result, 12)
+    }
+
+    #[test]
+    fn real_input_replace() {
+        let input = include_str!("../inputs/input.txt");
+        let result = process(input, Box::new(ReplaceNumberFinder {}));
 
         assert_eq!(result, 54980)
     }
---
+++ 2023/day-01/src/lib.rs
@@ -0,0 +1,79 @@
+static WORD_NUMBERS: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+static OVERLAPS: [(&'static str, &'static str); 7] = [
+    ("oneight", "oneeight"),
+    ("twone", "twoone"),
+    ("threeight", "threeeight"),
+    ("fiveight", "fiveeight"),
+    ("sevenine", "sevennine"),
+    ("eightwo", "eighttwo"),
+    ("nineight", "nineeight"),
+];
+
+pub trait NumberFinder {
+    fn find_number(&self, line: &str, from_end: bool) -> u32;
+}
+
+// Search substring from start/end until it contains a word number or digit
+// Harder to follow, but more efficient
+pub struct LinearSearchNumberFinder {}
+impl NumberFinder for LinearSearchNumberFinder {
+    fn find_number(&self, line: &str, from_end: bool) -> u32 {
+        let mut char_indices: Vec<(usize, char)> = line.char_indices().collect();
+        if from_end {
+            char_indices = char_indices.into_iter().rev().collect();
+        }
+
+        for (i, char) in char_indices {
+            // Check if there is a spelled out number
+            let word_number = WORD_NUMBERS.iter().find(|w| {
+                if from_end {
+                    line[i..].starts_with(*w)
+                } else {
+                    line[..i].ends_with(*w)
+                }
+            });
+            if let Some(word_number) = word_number {
+                let digit = WORD_NUMBERS.iter().position(|w| w == word_number).unwrap();
+                return digit as u32 + 1; // Add 1 because 0 indexed
+            }
+
+            // Check for digit
+            if char.is_numeric() {
+                return char.to_digit(10).unwrap();
+            }
+        }
+
+        panic!("Shouldn't happen. Invalid input - no numbers found")
+    }
+}
+
+// Replace word numbers with digits, then find digits
+// Easier to follow, but less efficient
+pub struct ReplaceNumberFinder {}
+impl NumberFinder for ReplaceNumberFinder {
+    fn find_number(&self, line: &str, from_end: bool) -> u32 {
+        let mut replaced_line = line.to_string();
+
+        // Replace any overlapping words with separate ones
+        for (k, v) in OVERLAPS {
+            replaced_line = replaced_line.replace(k, v);
+        }
+
+        // Replace any words with digits
+        for (i, w) in WORD_NUMBERS.iter().enumerate() {
+            replaced_line = replaced_line.replace(w, &(i + 1).to_string());
+        }
+
+        // Find digit
+        let char = if from_end {
+            replaced_line.chars().rfind(|c: &char| c.is_numeric())
+        } else {
+            replaced_line.chars().find(|c: &char| c.is_numeric())
+        };
+
+        char.unwrap().to_digit(10).unwrap()
+    }
+}
