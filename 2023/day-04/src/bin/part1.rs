44a2e169abd3f45e00a729417f1e75a145e3cce2
Jacob <hortonjacob1@gmail.com>
2023-12-04 18:05:43 +0000

    day 4!

---
+++ 2023/day-04/src/bin/part1.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let input = include_str!("../inputs/input.txt");
+    let result = process(input);
+    println!("{result}");
+}
+
+fn process(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for card in input.split('\n') {
+        if card.trim().is_empty() {
+            continue;
+        }
+
+        let (_, numbers) = card.split_once(": ").unwrap();
+        let (winning, mine) = numbers.split_once(" | ").unwrap();
+
+        let mine: Vec<_> = mine.split(' ').filter(|n| !n.trim().is_empty()).collect();
+        let mine_winning: Vec<_> = winning.split(' ').filter(|n| mine.contains(n)).collect();
+
+        if mine_winning.len() > 0 {
+            sum += 2u32.pow(mine_winning.len() as u32 - 1);
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../inputs/test_part1.txt");
+        let result = process(input);
+        assert_eq!(result, 13);
+    }
+}
---
+++ 2023/day-04/src/bin/part2.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("../inputs/input.txt");
+    let result = process(input);
+    println!("{result}");
+}
+
+fn process(input: &str) -> u32 {
+    let mut sum = 0;
+    let mut multipliers: HashMap<u32, u32> = HashMap::new();
+
+    for card in input.split('\n') {
+        if card.trim().is_empty() {
+            continue;
+        }
+
+        let (card, numbers) = card.split_once(": ").unwrap();
+        let (winning, mine) = numbers.split_once(" | ").unwrap();
+
+        let (_, card_no) = card.split_once(' ').unwrap();
+        let card_no: u32 = card_no.trim().parse().unwrap();
+
+        let mine: Vec<_> = mine.split(' ').filter(|n| !n.trim().is_empty()).collect();
+        let mine_winning: Vec<_> = winning.split(' ').filter(|n| mine.contains(n)).collect();
+
+        let this_multiplier = multipliers.get(&card_no).unwrap_or(&1).clone();
+        sum += this_multiplier; // Add the number of copies of this card that we have
+
+        if mine_winning.len() > 0 {
+            // Add extra cards to the ones after
+            for i in card_no + 1..card_no + 1 + mine_winning.len() as u32 {
+                let entry = multipliers.entry(i).or_insert(1);
+                *entry += this_multiplier;
+            }
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../inputs/test_part2.txt");
+        let result = process(input);
+        assert_eq!(result, 30);
+    }
+}
