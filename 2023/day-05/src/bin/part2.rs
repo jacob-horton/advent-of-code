7bec70f227a2ef0ff62b0d228390eb445a0ca713
Jacob <hortonjacob1@gmail.com>
2023-12-05 23:17:23 +0000

    chore: tidy, remove unnecessary end points

--- 2023/day-05/src/bin/part2.rs
+++ 2023/day-05/src/bin/part2.rs
@@ -20,45 +20,46 @@ fn seed_exists(seeds: &Vec<u64>, seed: u64) -> bool {
     false
 }
 
-// Get boundaries from each range in the location "realm"
-fn get_intersection_points(mappings: &Vec<Mapping>) -> Vec<u64> {
-    let mut intersection_points = Vec::new();
+// Get start of each range mapped to location numbers
+fn get_range_start_points(mappings: &Vec<Mapping>) -> Vec<u64> {
+    let mut start_points = Vec::new();
 
-    // Loop through each mapping, collecting the end points of each range
+    // Loop through each mapping, collecting the start of each range
     for mapping in mappings {
-        // Add source end points to intersection points
-        intersection_points.append(
+        // Add source start to starting points
+        start_points.append(
             &mut mapping
                 .mappings
                 .iter()
-                .map(|m| [m.source_start, m.source_start + m.range_length])
-                .flatten()
+                .map(|m| m.source_start)
                 .collect_vec(),
         );
 
-        // Map intersections to next "realm"
-        intersection_points = intersection_points
+        // Map starting points to next category
+        start_points = start_points
             .into_iter()
             .map(|i| mapping.map_value(i))
             .collect_vec();
     }
 
     // Order them and get unique values
-    intersection_points.sort();
-    intersection_points.into_iter().unique().collect_vec()
+    start_points.sort();
+    start_points.into_iter().unique().collect_vec()
 }
 
 fn process(input: &str) -> u64 {
     let (seeds, mappings) = parse_input(input);
 
-    // Get the intersection points which define the boundaries of mapping ranges.
-    // We know that the lowest location will be at the start of one of these boundaries
-    // as within the boundaries, it will only increase as you increase the input
-    let intersection_points = get_intersection_points(&mappings);
+    // Get starting points of each range of each mapping (mapped to location).
+    // We know that the lowest location will be at the start of one of the
+    // ranges, as within the ranges, it will only increase as you increase the
+    // input
+    let range_start_points = get_range_start_points(&mappings);
 
-    // Work back from location to seed to find the first location point
-    // that has a corresponding seed
-    for i in intersection_points {
+    // Work back from location to seed to find the first location point that
+    // has a corresponding seed. As it is sorted, the first one we come across
+    // will be the smallest
+    for i in range_start_points {
         let mut v = i;
 
         for mapping in mappings.iter().rev() {
