9519762515d5a4a896f91c103fca235f43011702
Jacob <hortonjacob1@gmail.com>
2023-12-13 09:55:07 +0000

    day 13!

---
+++ 2023/day-13/src/bin/part1.rs
@@ -0,0 +1,51 @@
+use day_13::{does_reflect, get_bit_patterns};
+
+fn main() {
+    let input = include_str!("../inputs/input.txt");
+    let result = process(input);
+    println!("{result}");
+}
+
+fn process(input: &str) -> u32 {
+    let patterns = get_bit_patterns(input);
+
+    let mut horiz_mirrors = 0u32;
+    let mut vert_mirrors = 0u32;
+    for pattern in &patterns {
+        // NOTE: this does not check the smudges are in the same position
+        for i in 1..pattern.vertical.len() {
+            if does_reflect(&pattern.vertical, i as u32, 0, true) {
+                vert_mirrors += i as u32;
+                break;
+            }
+        }
+
+        for i in 1..pattern.horizontal.len() {
+            if does_reflect(&pattern.horizontal, i as u32, 0, true) {
+                horiz_mirrors += i as u32;
+                break;
+            }
+        }
+    }
+
+    vert_mirrors + 100 * horiz_mirrors
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../inputs/test_part1.txt");
+        let result = process(input);
+        assert_eq!(result, 405);
+    }
+
+    #[test]
+    fn real_input() {
+        let input = include_str!("../inputs/input.txt");
+        let result = process(input);
+        assert_eq!(result, 37975);
+    }
+}
---
+++ 2023/day-13/src/lib.rs
@@ -0,0 +1,70 @@
+fn line_to_bit_pattern(line: &str) -> u32 {
+    let mut bit_pattern = 0;
+
+    for (i, char) in line.chars().rev().enumerate() {
+        if char == '#' {
+            bit_pattern += 2u32.pow(i as u32);
+        }
+    }
+
+    bit_pattern
+}
+
+pub fn does_reflect(pattern: &[u32], mirror: u32, smudges: u32, smudges_exact: bool) -> bool {
+    let mut smudges_left = smudges as i32;
+
+    for (l, r) in pattern[..mirror as usize]
+        .iter()
+        .rev()
+        .zip(&pattern[mirror as usize..])
+    {
+        if l != r {
+            if smudges_left <= 0 {
+                return false;
+            }
+
+            // 1 cell different (smudge)
+            let diff = (l ^ r).count_ones() as i32;
+            if diff <= smudges_left {
+                smudges_left -= diff;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if smudges_exact {
+        smudges_left == 0
+    } else {
+        true
+    }
+}
+
+pub struct BitPatterns {
+    pub horizontal: Vec<u32>,
+    pub vertical: Vec<u32>,
+}
+
+pub fn get_bit_patterns(input: &str) -> Vec<BitPatterns> {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|lines| {
+            let sub_lines: Vec<_> = lines.split('\n').collect();
+            let mut vertical = Vec::new();
+            for i in 0..sub_lines[0].len() {
+                let vert_str = sub_lines
+                    .iter()
+                    .map(|l| l.chars().nth(i).unwrap())
+                    .collect::<String>();
+
+                vertical.push(line_to_bit_pattern(&vert_str));
+            }
+
+            BitPatterns {
+                horizontal: lines.split('\n').map(line_to_bit_pattern).collect(),
+                vertical,
+            }
+        })
+        .collect()
+}
