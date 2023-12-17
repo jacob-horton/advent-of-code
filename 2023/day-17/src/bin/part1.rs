535aa55110717a7bb1badd598e287f7b9336f9eb
Jacob <hortonjacob1@gmail.com>
2023-12-17 17:35:12 +0000

    day 17 part 1 run in 13 minutes

--- 2023/day-17/src/bin/part1.rs
+++ 2023/day-17/src/bin/part1.rs
@@ -1,10 +1,17 @@
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::{
+    collections::{BinaryHeap, HashMap, HashSet},
+    rc::Rc,
+    time::Instant,
+};
+
+macro_rules! hashset {
+    [$($e:expr),* $(,)?] => {
+        HashSet::from_iter([$($e),*])
+    }
+}
 
 fn main() {
-    // 914 too high
     let input = include_str!("../inputs/input.txt");
-    // let input = include_str!("../inputs/test_part1.txt");
-    // let input = include_str!("../inputs/test.txt");
     let result = process(input);
     println!("{result}");
 }
@@ -74,39 +81,36 @@ fn heuristic(start: (i32, i32), end: (i32, i32)) -> u32 {
 }
 
 fn find_shortest_path(grid: &Vec<Vec<u32>>, start: (i32, i32), end: (i32, i32)) -> u32 {
-    // TODO: come up with heuristic. Must always be less or equal to true cheapest cost
-    // Also must never overestimate step cost (h(n+1) - h(n) <= real(n+1) - real(n))
-    let mut frontier = BinaryHeap::new();
-    frontier.push(NodeDist {
-        node: start,
-        last_move: (1, 0),
-        heuristic: heuristic(start, end),
-        dist: 0,
-    });
-    frontier.push(NodeDist {
-        node: start,
-        last_move: (0, 1),
-        heuristic: heuristic(start, end),
-        dist: 0,
-    });
+    let mut nodes = HashMap::new();
+    let start_nodes = hashset!(
+        Rc::new(NodeDist {
+            node: start,
+            last_move: (0, 1),
+            heuristic: heuristic(start, end),
+            dist: 0,
+        }),
+        Rc::new(NodeDist {
+            node: start,
+            last_move: (1, 0),
+            heuristic: heuristic(start, end),
+            dist: 0,
+        }),
+    );
 
-    let mut distances = HashMap::new();
-    for n in &frontier {
-        distances.insert(n.clone(), 0);
+    nodes.insert(start, start_nodes);
+
+    let mut frontier: BinaryHeap<Rc<NodeDist>> = BinaryHeap::new();
+    for ns in nodes.values() {
+        for n in ns {
+            frontier.push(n.clone());
+        }
     }
 
     let mut visited = HashSet::new();
-    let mut count = 0;
     while !frontier.is_empty() {
-        count += 1;
-        if count % 10000 == 0 {
-            println!("{:?}", frontier.peek().unwrap());
-            println!("{}", frontier.len());
-        }
         let curr = frontier.pop().unwrap();
 
         if curr.node == end {
-            println!("Done");
             return curr.dist;
         }
 
@@ -122,14 +126,6 @@ fn find_shortest_path(grid: &Vec<Vec<u32>>, start: (i32, i32), end: (i32, i32)) 
                 continue;
             }
 
-            // Explore 3 spaces perp to last direction
-            // TODO: maybe
-            // If at start, only 2 (as all in line will be same direction)
-            // let mut num = 3;
-            // if curr.node == start {
-            //     num -= 1;
-            // }
-
             for i in -3..=3 {
                 if i == 0 {
                     continue;
@@ -144,12 +140,11 @@ fn find_shortest_path(grid: &Vec<Vec<u32>>, start: (i32, i32), end: (i32, i32)) 
                     continue;
                 }
 
-                let new_dist = curr.dist + cost_between(&grid, curr.node, neighbour);
+                let new_dist = curr.dist + cost_between(grid, curr.node, neighbour);
                 let neighbour_node = NodeDist {
                     node: neighbour,
                     last_move: (
-                        // TODO: make better
-                        // Trying to get either (0,1) or (1,0)
+                        // Get either (0,1) or (1,0)
                         (neighbour.0 - curr.node.0).signum().abs(),
                         (neighbour.1 - curr.node.1).signum().abs(),
                     ),
@@ -163,15 +158,22 @@ fn find_shortest_path(grid: &Vec<Vec<u32>>, start: (i32, i32), end: (i32, i32)) 
                 }
 
                 let mut replace = true;
-                if let Some(d) = distances.get(&neighbour_node) {
-                    if new_dist >= *d {
-                        replace = false;
+                if let Some(ns) = nodes.get(&neighbour_node.node) {
+                    for n in ns {
+                        if n.last_move == neighbour_node.last_move && new_dist >= n.dist {
+                            replace = false;
+                        }
                     }
                 }
 
                 if replace {
-                    distances.insert(neighbour_node.clone(), new_dist);
-                    frontier.push(neighbour_node);
+                    let rc = Rc::new(neighbour_node);
+                    nodes
+                        .get_mut(&rc.node)
+                        .unwrap_or(&mut HashSet::new())
+                        .insert(rc.clone());
+
+                    frontier.push(rc.clone());
                 }
             }
         }
